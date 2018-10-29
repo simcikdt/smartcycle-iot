29b67b73449914fb5fd4663285e86f26dbee4df9
simcik <simcik@gmail.com>
2018-10-28 22:11:42 -0400

    Got Cadence metric working.

--- multi_ant_demo.py
+++ multi_ant_demo.py
@@ -36,11 +36,42 @@ class CadenceCallback(event.EventCallback):
             #hexValues = binascii.hexlify(msg.payload)
             #print( "id:{} value:{} type:{}".format(message.number, hexValues, type(msg) ) )
             print(hexValues)
-            cacheLocation = '/home/aws_cam/diskcachedir'
-            with Cache(cacheLocation) as cache:
-                cache[b'cadence'] = ord(msg.payload[8]) 
-                print('Cached cadence: {}'.format(cache[b'cadence']))
-        
+
+            curr_cad_rev_count = int(str(binascii.hexlify(msg.payload[8])) + str(binascii.hexlify(msg.payload[7])), 16)
+            print('Cadence Rev Count: {}'.format(curr_cad_rev_count))
+            curr_cad_event_time = int(str(binascii.hexlify(msg.payload[6])) + str(binascii.hexlify(msg.payload[5])), 16)
+            print('Cadence Event Time: {}'.format(curr_cad_event_time))
+
+            #Stop bit doesn't seem to exist for the Garmin cadence sensor
+            #print('Cadence Page ID: {} Stop Bit: {}'.format(ord(msg.payload[1]), ord(msg.payload[2])))
+
+            cache_location = '/home/aws_cam/diskcachedir'
+
+            with Cache(cache_location) as cache:
+                prev_cad_rev_count = cache.get(b'prevcadrevcount', curr_cad_rev_count - 1)
+                prev_cad_event_time = cache.get(b'prevcadevttime', curr_cad_event_time - 1)
+
+                cache[b'prevcadrevcount'] = curr_cad_rev_count
+                cache[b'prevcadevttime'] = curr_cad_event_time
+
+                delta_cad_rev_count = curr_cad_rev_count - prev_cad_rev_count
+                delta_cad_event_time = curr_cad_event_time - prev_cad_event_time
+
+                print('Cadence Delta Rev Count: {} , Cadence Delta Evt Time: {}'.format(delta_cad_rev_count, delta_cad_event_time))
+
+                #avoid divide-by-zero errors; only re-calculate speed if input values greater than zero
+
+                if delta_cad_event_time > 0:
+                    rpm = (delta_cad_rev_count * 60 * 1024)/delta_cad_event_time
+
+                    print('Calc Cadence [RPM]: {}'.format(str(rpm)))
+                    cache[b'cadence'] = str(rpm)
+                    print('Cached cadence (modified): {} \n =====\n'.format(cache[b'cadence']))
+                else:
+                    #save as a string to return a Truthy value
+                    cache[b'cadence'] = str(0)
+                    print('Cached cadence (no new data): {} \n =====\n'.format(cache[b'cadence']))
+
 
 class SpeedCallback(event.EventCallback):
     def process(self, msg):
@@ -56,7 +87,7 @@ class SpeedCallback(event.EventCallback):
             curr_speed_event_time = int(str(binascii.hexlify(msg.payload[6])) + str(binascii.hexlify(msg.payload[5])), 16)
             print('Speed Event Time (1024): {}'.format(curr_speed_event_time))
 
-            print('Page ID: {} Stop Bit: {}'.format(ord(msg.payload[1]), ord(msg.payload[2])))
+            print('Speed Page ID: {} Stop Bit: {}'.format(ord(msg.payload[1]), ord(msg.payload[2])))
 
             stop_bit = ord(msg.payload[2])
             #Only update speed value when stop bit = 0 which means bike is moving
@@ -182,4 +213,14 @@ with HRM(serial=SERIAL, netkey=NETKEY) as hrm:
         try:
             time.sleep(1)
         except KeyboardInterrupt:
+            cache_location = '/home/aws_cam/diskcachedir'
+            with Cache(cache_location) as cache:
+                cache[b'speed'] = 0
+                cache[b'prevspdrevcount'] = 0
+                cache[b'prevspdevttime'] = 0
+                cache[b'prevcadrevcount'] = 0
+                cache[b'prevcadevttime'] = 0
+                cache[b'cadence'] = 0
+                cache[b'heartrate'] = 0
+
             sys.exit(0)
